b38f71e73477391ab98d2fd45b0cb34bc5bb0e7a
Oliver White <78637182+GGOtto@users.noreply.github.com>
2026-01-22 10:33:06 -0800

    directory reorg

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,4 @@
-import GameContainer from "./components/GameContainer/GameContainer";
+import GameContainer from "./ui/GameContainer/GameContainer";
 import "./page.css";
 
 export default function Home() {
